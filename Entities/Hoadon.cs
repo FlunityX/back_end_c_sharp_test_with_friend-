95c4b21137c888d6b444b8e98bd0b1657c796f67
nguyễn mạnh <manharia123@gmail.com>
2024-05-08 00:08:34 +0700

    add49

---
+++ Entities/Hoadon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace back_end_c_sharp_test_with_friend_.Entities;
+
+public partial class Hoadon
+{
+    public string Mahoadon { get; set; } = null!;
+
+    public string Tenkhachhang { get; set; } = null!;
+
+    public DateTime? Ngaylap { get; set; }
+
+    public decimal? Tongtien { get; set; }
+
+    public virtual ICollection<Chitiethoadon> Chitiethoadons { get; set; } = new List<Chitiethoadon>();
+}
