3173f800b8357378c9232c4c58296675cd9ff98d
nguyễn mạnh <manharia123@gmail.com>
2024-05-08 00:12:06 +0700

    add54

---
+++ Entities/Loaisach.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace back_end_c_sharp_test_with_friend_.Entities;
+
+public partial class Loaisach
+{
+    public string Tenloaisach { get; set; } = null!;
+
+    public virtual ICollection<Sach> Saches { get; set; } = new List<Sach>();
+}
