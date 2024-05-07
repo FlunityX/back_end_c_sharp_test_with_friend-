6f3460a864c0c997a32553fc1d574d3deb446e1d
nguyễn mạnh <manharia123@gmail.com>
2024-05-08 00:12:25 +0700

    add57

---
+++ Entities/Nhaxuatban.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace back_end_c_sharp_test_with_friend_.Entities;
+
+public partial class Nhaxuatban
+{
+    public string Tennhaxuatban { get; set; } = null!;
+
+    public virtual ICollection<Sach> Saches { get; set; } = new List<Sach>();
+}
