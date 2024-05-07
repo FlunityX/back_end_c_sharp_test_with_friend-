4548a084eba121093790316b673d9bab55f80d10
nguyễn mạnh <manharia123@gmail.com>
2024-05-08 00:08:59 +0700

    add53

---
+++ Entities/Taikhoan.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace back_end_c_sharp_test_with_friend_.Entities;
+
+public partial class Taikhoan
+{
+    public string Username { get; set; } = null!;
+
+    public string PassWord { get; set; } = null!;
+}
