f88f0b3d7e245f7d88f796f1b4faaf1790033089
nguyễn mạnh <manharia123@gmail.com>
2024-05-08 00:12:18 +0700

    add56

---
+++ Entities/TblKhach.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace back_end_c_sharp_test_with_friend_.Entities;
+
+public partial class TblKhach
+{
+    public string MaKhach { get; set; } = null!;
+
+    public string? TenKhach { get; set; }
+
+    public string? DiaChi { get; set; }
+
+    public string? DienThoai { get; set; }
+}
