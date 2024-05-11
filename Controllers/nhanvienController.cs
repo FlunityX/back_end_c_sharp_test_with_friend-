a7e1feed07a1715ff3ba978087503b63eb101ddc
nguyễn mạnh <manharia123@gmail.com>
2024-05-11 12:03:07 +0700

    add18

---
+++ Controllers/nhanvienController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using quan_ly_ban_hang.Entities;
+
+namespace back_end_c_sharp_test_with_friend_.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class nhanvienController : Controller
+    {
+        private readonly DbAa8167Nguyenmanh1203Context _context;
+        public nhanvienController(DbAa8167Nguyenmanh1203Context ctx)
+        {
+            _context = ctx;
+        }
+
+        [HttpGet]
+        [Route("Trả về thông tin tất cả nhân viên Theo khu vực")]
+        public async Task<IActionResult> GetNhanvien(int MaKhuVuc)
+        {
+            var nhanvienList = await _context.TblNhanViens
+                .Where(nv => nv.MaKvNavigation.MaKv == MaKhuVuc)
+                .ToListAsync();
+            
+            return Ok(nhanvienList);
+        }
+        [HttpHead]
+        public IActionResult GetAllHead()
+        {
+            // Trả về một phản hồi chứa nội dung tùy chỉnh
+            return Ok();
+        }
+    }
+}
